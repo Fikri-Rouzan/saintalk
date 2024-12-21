49b68f25aa1039d293515e0c357609995234077d
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2024-12-21 22:05:11 +0700

    second commit

--- resources/views/pages/app/report/create.blade.php
+++ resources/views/pages/app/report/create.blade.php
@@ -91,7 +91,7 @@
         </div>
 
         <button class="btn btn-primary w-100 mt-2" type="submit" color="primary">
-            Laporkan
+            Report
         </button>
     </form>
 @endsection
